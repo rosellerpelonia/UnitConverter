6002524b32a103923eb4bdb9a5d48d2803735368
Roseller Pelonia <roseller.pelonia@gmail.com>
2024-06-15 17:42:30 +0800

    padding vs spacer

--- app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -10,7 +10,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
@@ -23,6 +26,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.unitconverter.ui.theme.UnitConverterTheme
 
 class MainActivity : ComponentActivity() {
@@ -50,10 +54,13 @@ fun UnitConverter() {
         horizontalAlignment = Alignment.CenterHorizontally
     ){
         // Here all the UI elements will be stacked below each other
-        Text("Unit Converter")
+        Text("Unit Converter", modifier = Modifier.padding(16.dp))
+        Spacer(modifier = Modifier.height(16.dp))
         OutlinedTextField(value = "", onValueChange = {
             //Here goes what should happen, when the value changed})
         })
+        Spacer(modifier = Modifier.height(16.dp))
+
 
         Row {
             // Here all the UI elements will be stacked below each other
@@ -75,6 +82,7 @@ fun UnitConverter() {
 
             }
         }
+        Spacer(modifier = Modifier.height(16.dp))
         Text("Result: ")
     }
 
