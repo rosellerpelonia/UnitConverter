823afd9c0cab6667792c6167c726df0b05a4c7d2
Roseller Pelonia <roseller.pelonia@gmail.com>
2024-06-17 16:50:10 +0800

    Implementing State in OutlinedTextField

--- app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -21,16 +21,23 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableDoubleStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.unitconverter.ui.theme.UnitConverterTheme
+import java.io.File
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,6 +58,14 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun UnitConverter() {
 
+    var inputValue by remember { mutableStateOf("") }
+    var outputValue by remember { mutableStateOf("") }
+    var inputUnit by remember { mutableStateOf("Centimeters") }
+    var outputUnit by remember { mutableStateOf("Meters") }
+    var iExpanded by remember { mutableStateOf(false) }
+    var oExpanded by remember { mutableStateOf(false) }
+    var conversionFactor = remember { mutableDoubleStateOf(0.01) }
+
     Column (
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
@@ -59,21 +74,23 @@ fun UnitConverter() {
         // Here all the UI elements will be stacked below each other
         Text("Unit Converter")
         Spacer(modifier = Modifier.height(16.dp))
-        OutlinedTextField(value = "", onValueChange = {
-            //Here goes what should happen, when the value changed})
-        })
+        OutlinedTextField(value = inputValue, onValueChange = {
+           inputValue = it
+        },
+           label = { Text(text = "Enter Value")} )
         Spacer(modifier = Modifier.height(16.dp))
 
 
         Row {
             // Here all the UI elements will be stacked below each other
-            Box{
+            Box {
                 Button(onClick = { /*TODO*/ }) {
                     Text("Select")
                     Icon(Icons.Default.ArrowDropDown, contentDescription = "Arrow Down")
                 }
 
-                DropdownMenu(expanded = true, onDismissRequest = { /*TODO*/ }) {
+                DropdownMenu(expanded = false,
+                    onDismissRequest = { }) {
                     DropdownMenuItem(
                         text = { Text(text = "Centimeters")},
                         onClick = { /*TODO*/ })
@@ -96,10 +113,11 @@ fun UnitConverter() {
                     Text("Select")
                     Icon(Icons.Default.ArrowDropDown, contentDescription = "Arrow Down")
                 }
-                DropdownMenu(expanded = true, onDismissRequest = { /*TODO*/ }) {
+                DropdownMenu(expanded = false, onDismissRequest = { /*TODO*/ }) {
                     DropdownMenuItem(
                         text = { Text(text = "Centimeters")},
-                        onClick = { /*TODO*/ })
+                        onClick = { /*TODO*/ },)
+
                     DropdownMenuItem(
                         text = { Text(text = "Meters")},
                         onClick = { /*TODO*/ })
